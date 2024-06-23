2da90f4729c37c91b4569eff61071d7ed5501116
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-06-23 17:42:34 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import './App.css';
-import Signup from './Components/signUp';
-import Login from './Components/Login';
-import PdfValidation from './Components/pdfValidation'; // Ensure correct import path and casing
+import React, { useState } from "react";
+import "./App.css";
+import Signup from "./Components/signUp";
+import Login from "./Components/Login";
+import PdfValidation from "./Components/pdfValidation"; // Ensure correct import path and casing
 
 function App() {
   const [isRegistered, setIsRegistered] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [forgotPasswordUsername, setForgotPasswordUsername] = useState('');
+  const [forgotPasswordUsername, setForgotPasswordUsername] = useState("");
 
   const handleRegister = () => {
     setIsRegistered(true);
@@ -45,7 +45,10 @@ function App() {
       </nav>
 
       {!isLoggedIn && !isRegistered && (
-        <Login onLogin={handleSuccessfulLogin} onForgotPassword={handleForgotPassword} />
+        <Login
+          onLogin={handleSuccessfulLogin}
+          onForgotPassword={handleForgotPassword}
+        />
       )}
       {!isLoggedIn && isRegistered && <Signup onRegister={handleRegister} />}
       {isLoggedIn && <PdfValidation />}
