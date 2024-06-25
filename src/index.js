eff983eb3d5120c6ab5e862633122b0e54110c73
Nishchey khajuria <70341267+nishcheyk@users.noreply.github.com>
2024-06-26 01:04:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
->>>>>>> abhinav
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
