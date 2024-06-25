be333e6dd4c3f9d4a2a16b0b6560bec2ec49d90f
Nishchey khajuria <70341267+nishcheyk@users.noreply.github.com>
2024-06-26 01:05:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import "./App.css";
-import Signup from "./Components/signUp";
-import Login from "./Components/Login";
-import PdfValidation from "./Components/pdfValidation"; // Ensure correct import path and casing
-
-function App() {
-  const [isRegistered, setIsRegistered] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-
-  const handleRegister = () => {
-    setIsRegistered(true);
-  };
-
-
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
-  const handleSuccessfulLogin = () => {
-    setIsLoggedIn(true);
-    setIsRegistered(false);
-  };
-
-
-  return (
-    <div className="App">
-      <nav>
-        {!isLoggedIn && (
-          <>
-            <button onClick={handleRegister}>Signup</button>
-            <button onClick={() => setIsRegistered(false)}>Login</button>
-          </>
-        )}
-        {isLoggedIn && <button onClick={handleLogout}>Logout</button>}
-      </nav>
-
-      {!isLoggedIn && !isRegistered && (
-        <Login
-          onLogin={handleSuccessfulLogin}
-
-        />
-      )}
-      {!isLoggedIn && isRegistered && <Signup onRegister={handleRegister} />}
-      {isLoggedIn && <PdfValidation />}
-    </div>
-=======
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -61,7 +11,6 @@ function App() {
         <Route path="/login" element={<LoginPage/>} />
       </Routes>
 
->>>>>>> abhinav
   );
 }
 
