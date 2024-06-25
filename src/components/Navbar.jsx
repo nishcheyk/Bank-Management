a053498600a2c5fbf9f79926f6bb9b19efd5d1cc
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-06-25 22:30:20 +0530

    try fixing error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,82 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-
-    let navigate = useNavigate();
-    const handleClick = () => {
-      navigate('/login');
-    }
+  let navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/login");
+  };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <nav
+      className="navbar navbar-expand-lg bg-body-tertiary"
+      data-bs-theme="dark"
+    >
       <div className="container-fluid">
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item mx-5">
-              <a className="nav-link active" aria-current="page" href="">Home</a>
+              <a className="nav-link active" aria-current="page" href="">
+                Home
+              </a>
             </li>
             <li className="nav-item mx-5">
-              <a className="nav-link" href="/login" onClick={handleClick}>Login</a>
+              <a className="nav-link" href="/login" onClick={handleClick}>
+                Login
+              </a>
             </li>
             <li className="nav-item mx-5">
-              <a href="#" className="nav-link">Sign-Up</a>
+              <a href="" className="nav-link">
+                Sign-Up
+              </a>
             </li>
             <li className="nav-item dropdown mx-5">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a
+                className="nav-link dropdown-toggle"
+                href=""
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 More Actions
               </a>
               <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider"/></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li>
+                  <a className="dropdown-item" href="">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="">
+                    Something else here
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
           </form>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
